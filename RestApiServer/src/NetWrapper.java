5f0a87db257f8251768a3fdc4cb85293ef804166
DmitryMI <wet75787@tqosi.com>
2019-05-19 17:40:45 +0300

    NetWrapper added

--- RestApiServer/src/NetWrapper.java
+++ RestApiServer/src/NetWrapper.java
@@ -1,4 +1,30 @@
 public class NetWrapper
 {
+    private Callback postCallback, getCallback;
 
+    public NetWrapper(Callback postCallback, Callback getCallback)
+    {
+        this.postCallback = postCallback;
+        this.getCallback = getCallback;
+    }
+
+    public static class NetData
+    {
+
+    }
+
+    public static class SenderInfo
+    {
+
+    }
+
+    public interface Callback
+    {
+        void OnDataReceived(NetData data, SenderInfo sender);
+    }
+
+    public void SendPackage(NetData data)
+    {
+        
+    }
 }
